e2504a8205f99701a4472b23a9d8c439104d52f6
Dwi Darma <dwikbtn15@gmail.com>
2021-01-11 15:00:48 +0800

    preparing the project for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,40 +1,9 @@
 import React, { Component } from "react";
-// import axios from 'axios';
-import axios from "../../axios";
+import Post from "./Posts/Posts";
 
 import "./Blog.css";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false,
-  };
-
-  componentDidMount() {
-    axios
-      .get("/posts")
-      .then((response) => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map((post) => {
-          return {
-            ...post,
-            author: "Max",
-          };
-        });
-        this.setState({ posts: updatedPosts });
-        // console.log( response );
-      })
-      .catch((error) => {
-        // console.log(error);
-        this.setState({ error: true });
-      });
-  }
-
-  postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
-  };
-
   render() {
     return (
       <div className="Blog">
@@ -48,6 +17,7 @@ class Blog extends Component {
             </li>
           </ul>
         </header>
+        <Post />
       </div>
     );
   }
